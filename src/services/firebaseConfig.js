3bed96d58ab1d35b812a4b5a588509bbae7e9126
leandrop22 <leandropetricca123@gmail.com>
2025-05-02 16:44:37 -0300

    commit 8

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -2,16 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyBEfdg1Yr5TAF9zZKLdxhnEBlu6hmVe-YM",
-    authDomain: "movilstore-a2d16.firebaseapp.com",
-    projectId: "movilstore-a2d16",
-    storageBucket: "movilstore-a2d16.firebasestorage.app",
-    messagingSenderId: "1076468642450",
-    appId: "1:1076468642450:web:742f4b4e0ef62d6d2f2c4b"
-  };
-
 const app = initializeApp(firebaseConfig);
 
 // Exportamos los servicios que vamos a usar
